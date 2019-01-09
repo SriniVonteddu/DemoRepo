5e60bfcdfc0e340dd2f6fb371e4d800efb526a3c
srini <srinuv29@gmail.com>
2019-01-09 14:04:33 -0700

    added New GitDemoRepo project

---
+++ src/test/java/com/qa/testcases/TestCase1.java
@@ -0,0 +1,13 @@
+package com.qa.testcases;
+
+import org.junit.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void loginTest() {
+		System.out.println("This is Git Repo Demo Class");
+	
+	}
+
+}
