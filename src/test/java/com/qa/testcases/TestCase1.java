c05f99ccf079e4d26ee1accb202f311a053efc8c
srini <srinuv29@gmail.com>
2019-01-09 16:01:48 -0700

    Added login Page class

--- src/test/java/com/qa/testcases/TestCase1.java
+++ src/test/java/com/qa/testcases/TestCase1.java
@@ -3,9 +3,13 @@ package com.qa.testcases;
 import org.junit.Test;
 
 public class TestCase1 {
-
+	
+	WebDriver driver;
+	
 	@Test
 	public void loginTest() {
+		driver = new WebDriver();
+		
 		System.out.println("This is Git Repo Demo Class");
 	
 	}
